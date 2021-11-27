e863f71275042a9f5d7ea469c87e6a0bcd32414a
Saurabh <saurabh160196@gmail.com>
2021-11-28 01:30:09 +0530

    adding auth guard for admin page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { RequestsComponent } from './requests/requests.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'admin', component: RequestsComponent},
+  {path: 'admin', canActivate: [AuthGuard],component: RequestsComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'account', loadChildren: () => import('./account/account.module')
       .then(mod => mod.AccountModule)},
